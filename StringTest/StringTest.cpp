9c6f2515ae90c8f0490d243e4986f78abfeea2e9
Anna Kapochkina <75569458+avkapochkina@users.noreply.github.com>
2022-10-18 19:30:33 +0300

    Initial commit

--- StringTest/StringTest.cpp
+++ StringTest/StringTest.cpp
@@ -1,6 +1,15 @@
+#include <iostream>
+using namespace std;
 
-int main(int argc, char* argv[])
+int main ()
 {
-    
+    string s;
+    cin >> s;
+    for(auto i = s.begin(); i != s.end(); i++)
+    {
+        if(count(s.begin(), s.end(), *i) == 1)
+            cout << '(';
+        else cout << ')';
+    }
     return 0;
 }
